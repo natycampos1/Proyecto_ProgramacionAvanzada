1edc7c2ec90ffda3ad23ea729c220b2c5666a974
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-02-19 20:48:05 -0600

    Login
    Modelo y formulario completos

--- KN_WEB/models/LoginModel.cs
+++ KN_WEB/models/LoginModel.cs
@@ -1,7 +1,17 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace KN_WEB.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "La identificación es obligatoria")]
+        public string Identificacion { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        public string Contrasenna { get; set; }
+
+        public bool Remember { get; set; }
     }
 }
